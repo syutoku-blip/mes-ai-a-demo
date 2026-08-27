399ec5b2685fd9a12bd2b04decc2f0e995f8ea8e
syutoku-blip <syutoku@worldshift-inc.com>
2026-08-27 13:33:46 +0900

    Create config.js

---
+++ dev2/config.js
@@ -0,0 +1,11 @@
+/**
+ * Allynair Frontend Configuration
+ *
+ * Google Apps Script をウェブアプリとしてデプロイした後、
+ * 発行された /exec URL を GAS_WEB_APP_URL に設定してください。
+ */
+
+window.ALLYNAIR_CONFIG = {
+  GAS_WEB_APP_URL:
+    'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec'
+};
